c817c3bd60b42ae2d46ad8380dcd33e72e6788db
Jeremy Zimmerman <stopspazzing@gmail.com>
2017-04-20 11:09:13 -0700

    small fixes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
 import os
 from sanic import Sanic
+from sanic.exceptions import NotFound
 from sanic_useragent import SanicUserAgent
 from sanic_compress import Compress
-from sanic.response import text, file, html, redirect
+from sanic.response import text, html, redirect
 from sanic_session import InMemorySessionInterface
 
 app = Sanic(__name__)
@@ -26,19 +27,12 @@ async def save_session(request, response):
     # pass the response to set client cookies
     await session_interface.save(request, response)
 
+
+@app.exception(NotFound)
+async def ignore_404s(request, exception):
+    return text("Yep, I totally found the page: {}".format(request.url))
+
 # Define the handler functions
-async def index(request):
-    return await file('index.html')
-
-async def images(request, name):
-    return await file('images\\' + name)
-
-async def styles(request):
-    return await file('styles.css')
-
-async def entry(request):
-    return await file('entry.html')
-
 async def login(request):
     cookie_check = request.cookies.get('session')
     # Start with html5
@@ -61,20 +55,17 @@ async def login(request):
     html_code += '</html>'
     return h
 
-async def logout(request):
-    o = html('<h1>Logging out %s</h1>' % request['session']['username'])
-    return o
-
 async def redirect_index(request):
     return redirect('/')
 
+# Add static routes
+app.static('/', './index.html')
+app.static('/entry.html', './entry.html')
+app.static('/images', './images')
+app.static('/styles.css', './styles.css')
+
 # Add each handler function as a route
-app.add_route(index, '/')
-app.add_route(images, '/images/<name>')
-app.add_route(styles, '/styles.css')
-app.add_route(entry, 'entry.html')
 app.add_route(login, '/login', methods=['GET', 'POST'])
-app.add_route(logout, '/logout')
 app.add_route(redirect_index, '/index.html')
 
 if __name__ == '__main__':
