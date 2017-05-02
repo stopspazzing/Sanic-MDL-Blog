fcdd16a0164737eb34aece3e9d823e934331e1b0
Jeremy Zimmerman <stopspazzing@gmail.com>
2017-05-02 14:52:54 -0700

    added sqlite3 db creation. moved all config settings to before server start. still need to add demo content to db and read from it.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sqlite3
 from sanic import Sanic
 from sanic.exceptions import NotFound
 from sanic_useragent import SanicUserAgent
@@ -18,10 +19,41 @@ jinja = SanicJinja2(app)
 Compress(app)
 session = InMemorySessionInterface(expiry=600)
 SanicUserAgent.init_app(app, default_locale='en_US')
-app.config['SECRET_KEY'] = os.urandom(24)
 
 
-# TODO: Add DB info
+@app.listener('before_server_start')
+async def setup_cfg(app, loop):
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['DATABASE'] = 'database.db'
+    app.config['DEMO_CONTENT'] = True
+    try:
+        cfg = app.config.from_pyfile('config.cfg')
+        if cfg is None:
+            app.config.from_envvar('config_file')
+    except FileNotFoundError:
+        print('Error - Config Not Found')
+    app.db = sqlite3.connect(app.config['DATABASE'])
+    app.db.execute(
+        '''CREATE TABLE IF NOT EXISTS blog_data(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL default 'None', post_author UNSIGNED BIG INT(20) NOT NULL default '0', post_date DATETIME NOT NULL default '0000-00-00 00-00-00', post_content TEXT NOT NULL default 'None', post_title TEXT NOT NULL default 'None', post_excerpt TEXT NOT NULL default 'None', post_status VARCHAR(20) NOT NULL default 'publish', post_modified DATETIME NOT NULL, comment_status VARCHAR(20) NOT NULL default 'open', post_password VARCHAR(20) NOT NULL, post_name VARCHAR(200) NOT NULL, post_likes VARCHAR(20) NOT NULL)''')
+    if app.config['DEMO_CONTENT']:
+        app.db.execute(
+            '''CREATE TABLE IF NOT EXISTS blog_demo(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL default 'None', post_author VARCHAR(20) NOT NULL default 'demo', post_date DATETIME NOT NULL default '0000-00-00 00-00-00', post_content TEXT NOT NULL default 'None', post_title TEXT NOT NULL default 'None', post_excerpt TEXT NOT NULL default 'None', post_status VARCHAR(20) NOT NULL default 'publish', post_modified DATETIME NOT NULL default '0000-00-00 00-00-00', comment_status VARCHAR(20) NOT NULL default 'open', post_password VARCHAR(20) NOT NULL default 'None', post_name VARCHAR(200) NOT NULL default 'new post', post_likes VARCHAR(20) default '0')''')
+        #app.db.cursor().execute('''INSERT INTO blog_demo (post_author,post_date,post_content,post_title,post_excerpt,post_name,post_likes) VALUES ('demo','2017-05-02 14:38:12.228329','this is test content filler for your enjoyment','test','this is test content filler...','test','2');''')
+
+
+@app.listener('after_server_start')
+async def notify_server_started(app, loop):
+    print('Server successfully started!')
+
+
+@app.listener('before_server_stop')
+async def notify_server_stopping(app, loop):
+    print('Server shutting down!')
+
+
+@app.listener('after_server_stop')
+async def close_db(app, loop):
+    app.db.close()
 
 
 @app.middleware('request')
@@ -42,7 +74,6 @@ async def ignore_404s(request, exception):
     page['text'] = 'We Can\'t Seem To Find ' + request.url
     return jinja.render('page.html', request, page=page)
 
-
 wtf = SanicWTF(app)
 
 
@@ -51,8 +82,8 @@ class LoginForm(wtf.Form):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
 
-
 async def index(request):
+    # TODO: Need to pull posts and their excerpts then order them by date
     return jinja.render('index.html', request)
 
 
@@ -123,7 +154,6 @@ async def logout(request):
 
 async def redirect_index(request):
     return redirect('/')
-
 
 app.add_route(index, '/')
 app.add_route(images, 'images/<name>')
